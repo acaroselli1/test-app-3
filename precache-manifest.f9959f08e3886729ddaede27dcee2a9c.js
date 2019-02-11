314cdb57009d72d4f12a4a4dc7f3b00bc195bf8c
Alexander Caroselli <alexandersylvia9@msn.com>
2019-02-11 15:39:47 -0700

    Updates

---
+++ precache-manifest.f9959f08e3886729ddaede27dcee2a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app-3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af4b911bd8ed64228c27",
+    "url": "/test-app-3/static/js/runtime~main.af4b911b.js"
+  },
+  {
+    "revision": "7b14a5d76f840095310c",
+    "url": "/test-app-3/static/js/main.7b14a5d7.chunk.js"
+  },
+  {
+    "revision": "d16a266844ec01aafc8a",
+    "url": "/test-app-3/static/js/2.d16a2668.chunk.js"
+  },
+  {
+    "revision": "7b14a5d76f840095310c",
+    "url": "/test-app-3/static/css/main.d97e546a.chunk.css"
+  },
+  {
+    "revision": "efec3dcbe07a06551abd1efd5ab9ee25",
+    "url": "/test-app-3/index.html"
+  }
+];
